671442e0b6cb336bdaa9b78b2fcc9a07ddce68a4
Tom Tobar <tomas.tobar@flatironschool.com>
2023-03-07 10:38:54 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+const destructivelyAppendCat = (name) => {
+    cats.push(name)
+}
+const destructivelyPrependCat = (name) => {
+    cats.unshift(name)
+}
+const destructivelyRemoveFirstCat = () => {
+    cats.shift()
+}
+
+const destructivelyRemoveLastCat = () => {
+    cats.pop()
+}
+
+const appendCat = (name) => {
+    return [...cats, name]
+}
+
+const prependCat = (name) => {
+    return [name, ...cats]
+}
+
+const removeLastCat = () => {
+     const catsCpy = [...cats]
+    catsCpy.pop()
+    return catsCpy
+}
+const removeFirstCat = () => {
+     const catsCpy = [...cats]
+    catsCpy.shift()
+    return catsCpy
+}
